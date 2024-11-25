f1390a1a7b51766d3f4a5f7cb4a6615f0d961c40
Example <you@example.com>
2024-11-25 15:03:07 -0500

    Add example

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send(`
+    <!doctype html>
+
+    <body>
+      <form action="/result">
+        <label for="input">Enter number: </label>
+        <input type="number" name="numberInput"></input>
+        <input type="submit" value="OK"></input>
+      </form>
+    </body>
+    
+
+  `);
+});
+
+router.get('/result', function(req, res, next) {
+  console.log(req);
+  let addedNumber = Number(req.query.numberInput) * 2;
+  // res.render('index', { title: 'Express' });
+  res.send(`
+    <!doctype html>
+
+    <body>
+      <p>The result is: ${addedNumber} </p>
+    </body>
+  `);
 });
 
 module.exports = router;
